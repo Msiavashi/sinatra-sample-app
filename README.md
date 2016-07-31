e7f61459c89243b6ab4db47a542ab2337a7dd38b
MSiavashi <mohammad.siav@hotmail.com>
2016-07-31 07:47:01 +0430

    added database support

--- README.md
+++
@@ -1,7 +0,0 @@
-# sinatra-sample-app
-
-this is a simple TODO list app written with ruby sinatra , slim (template engine) , sqlite (as database) and data_mapper as ORM .
-
-the app is meant to be used as sample for future usages .
-
-this app is based on this tutorial : https://www.sitepoint.com/just-do-it-learn-sinatra-ii-2/
--- sinatraWebServer.rb
+++ sinatraWebServer.rb
@@ -1,7 +1,21 @@
 require 'sinatra'
 require 'slim'
+require 'data_mapper'
 
+# the part below handles the database connections
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+class Task
+  include DataMapper::Resource      # this shows that the class is a data_mapper class
+  property :id,           Serial
+  property :name,         String, :required => true
+  property :completed_at, DateTime
+end
+DataMapper.finalize
+
+
+# the part below handles the requests
 get '/' do
+  @tasks = Task.all
   slim :index
 end
 
@@ -11,6 +25,20 @@ get '/:task' do
 end
 
 post '/' do
-  @task = params[:task]
-  slim :task
+  Task.create :name => params[:task]
+  # @task = params[:task]
+  # slim :task
+  redirect to('/')
+end
+
+delete '/task/:id' do
+  Task.get(params[:id]).destroy
+  redirect to('/')
+end
+
+put '/task/:id' do
+  task = Task.get params[:id]
+  task.completed_at = task.completed_at.nil? ? Time.now : nil
+  task.save
+  redirect to('/')
 end
